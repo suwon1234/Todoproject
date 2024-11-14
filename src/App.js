9ea80a2e621406f77297fe4bfd82cae89e3b401a
suwon1234 <psj04100410262635@gmail.com>
2024-11-14 21:52:19 +0900

    day01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import GlobalStyle from './global/global';
+import { ThemeProvider } from 'styled-components';
+import theme from './global/theme';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router';
 
 function App() {
   return (
-   <></>
+   <>
+    <ThemeProvider theme={theme}>
+    <GlobalStyle/>
+    <RouterProvider router={router}/>
+    </ThemeProvider>
+   
+   
+   </>
   );
 }
 
---
+++ src/modules/todo.js
@@ -0,0 +1,12 @@
+import {createAction,handleActions} from 'redux-actions'
+
+const todoInitValue={
+
+
+}
+
+const todo=handleActions({
+
+},todoInitValue)
+
+export default todo
